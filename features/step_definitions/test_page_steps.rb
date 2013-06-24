5536135a80e2a5b5b465ba9ac9937318e4d2936e
Akash Khadelwal <akash.khandelwal@jombay.com>
2013-06-24 19:28:49 +0530

    Test to check all questions are answered per page

--- features/step_definitions/test_page_steps.rb
+++ features/step_definitions/test_page_steps.rb
@@ -3,11 +3,21 @@ When(/^I go to test page$/) do
   
 end
 
-Then(/^I click "(.*?)" button without answering all questions$/) do |arg1|
-  page.choose('option_4802')
-	select "3", :from => "options[item_1489_1489]"
+Then(/^I click next button without answering all questions$/) do
+  page.first(".radio").click
   click_button('Next')
 end
+
+Then(/^I click next button after answering all the questions$/) do
+	page.all(".radio input[type = 'radio']").each{|o| o.click}
+	click_button('Next')
+  # find(:select, field).all('option').find{|o| o.value==value}.select_option
+end
+
+Then(/^I see no error message$/) do
+  page.should_not have_content('All the questions are mandatory, Looks like you had missed out to answer some questions')
+end
+
 
 Then(/^I get error message$/) do
   page.should have_content('All the questions are mandatory, Looks like you had missed out to answer some questions')
--- features/test_page.feature
+++ features/test_page.feature
@@ -5,6 +5,7 @@ I want to check if questions are answered
 	Scenario: Visit test page
 		When I go to test page
 			Then I should see "Set 1"
-			Then I click "Next" button without answering all questions
+			Then I click next button without answering all questions
 			Then I get error message
-			
+			Then I click next button after answering all the questions
+			Then I see no error message
